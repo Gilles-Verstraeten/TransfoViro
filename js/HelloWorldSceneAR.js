bfedc1fd4d62ef980e0e151dd3deccdf4e08b0d0
Louis Moerman <louis.moerman@student.vives.be>
2020-02-07 12:00:54 +0100

    'commit'

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -35,8 +35,11 @@ export default class HelloWorldSceneAR extends Component {
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized}>
-        <Viro3DObject
+      <ViroARScene onTrackingUpdated={this._onInitialized} >
+        <ViroDirectionalLight color="#ffffff" direction={[0,-1,-.2]}/>
+      <ViroAmbientLight color="#ffffff" intensity={200}/>
+
+      <Viro3DObject
         source={require('./res/icecreamman_anim/icecreamman_anim_a.vrx')}
         resources={[require('./res/icecreamman_anim/icecreamman_diffuse.png'),
                     require('./res/icecreamman_anim/icecreamman_normal.png'),
@@ -44,80 +47,18 @@ export default class HelloWorldSceneAR extends Component {
         position={[0, -1, -2]}
         scale={[.5, .5, .5]}
         type="VRX"
-        //dragType="FixedToWorld" onDrag={()=>{}}
-        //onClick={this._onTappedIcecream}
-        //animation={{name:"02", run:this.state.runAnimation, loop:true,}}
+        dragType="FixedToWorld" onDrag={()=>{}}
+        onClick={this._onTappedIcecream}
+        animation={{name:"02", run:this.state.runAnimation, loop:true,}}
       />
       </ViroARScene>
-      /*<ViroARScene onTrackingUpdated={this._onInitialized}>
-        <Viro3DObject
-        source={require('./res/watch_tower/wooden_watch_tower2.obj')}
-        resources={[require('./res/watch_tower/textures/wood_tower_col.png'),
-                    require('./res/watch_tower/textures/wood_tower_nor.png')]}
-        //materials={[require('./res/watch_tower/wooden watch tower2.mtl')]}
-        position={[0, -1, -2]}
-        scale={[.5, .5, .5]}
-        type="OBJ"
-      />
-      </ViroARScene>*/
-      /*<ViroARScene onTrackingUpdated={this._onInitialized} >
-        <Viro3DObject
-          source={require('./res/emoji_smile/emoji_smile.vrx')}
-          resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-              require('./res/emoji_smile/emoji_smile_normal.png'),
-              require('./res/emoji_smile/emoji_smile_specular.png')]}
-          position={[0, 0, -1]}
-          scale={[.2, .2, .2]}
-          type="VRX" />
-      </ViroARScene>
-      <Viro3DObject 
-              source={require('./res/IphoneX/Iphone_seceond_version_finished.obj')}
-              resources={require('./res/IphoneX/Iphone_seceond_version_finished.mtl')}
-              position={[-0.0, -5.5, -1.15]}
-              //materials={["heart"]}
-              type="OBJ" />
-      */
-      
-      /*<ViroARScene>
-      <ViroDirectionalLight color="#ffffff" direction={[0,-1,-.2]}/>
-      <ViroAmbientLight color="#ffffff" intensity={200}/>
-      <Viro3DObject 
-              source={require('./res/IphoneX/Iphone_seceond_version_finished.obj')}
-              //resources={[require('./res/IphoneX/Iphone_seceond_version_finished.mtl'),
-              //require('./res/IphoneX/camera-lens-icon.jpg'),
-              //require('./res/IphoneX/iphone-x-creens-status-bar.jpg')]}
-              position={[0.0, 0.0, -1]}
-              scale={[0.1, 0.1, 0.1]}
-              type="OBJ"/>
-
-      </ViroARScene>*/
-      /*<ViroARScene onTrackingUpdated={this._onInitialized}>
-      <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      <ViroAmbientLight color={"#aaaaaa"} />
-      <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
-        position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-      <Viro3DObject
-          source={require('./res/emoji_smile/emoji_smile.vrx')}
-          resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-              require('./res/emoji_smile/emoji_smile_normal.png'),
-              require('./res/emoji_smile/emoji_smile_specular.png')]}
-          position={[-.5, .5, -1]}
-          scale={[.2, .2, .2]}
-          type="VRX" />
-      <Viro3DObject source={require('./res/bugatti.obj')}
-              resources={require('./res/bugatti.mtl')}
-              position={[0, -1, -2]}
-              scale={[.2, .2, .2]}
-              materials={[require('./res/bugatti.mtl')]}
-              type="OBJ" />
-      </ViroARScene>*/
     );
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Lelijk gezicht"
+        text : "Test"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -139,16 +80,16 @@ var styles = StyleSheet.create({
 module.exports = HelloWorldSceneAR;
 
 
-/*
-<Viro3DObject
-        source={require('./res/icecreamman_anim/icecreamman_anim_a.vrx')}
-        resources={[require('./res/icecreamman_anim/icecreamman_diffuse.png'),
-                    require('./res/icecreamman_anim/icecreamman_normal.png'),
-                    require('./res/icecreamman_anim/icecreamman_specular.png')]}
+
+
+/*</ViroARScene><ViroARScene onTrackingUpdated={this._onInitialized}>
+        <Viro3DObject
+        source={require('./res/watch_tower/wooden_watch_tower2.obj')}
+        resources={[require('./res/watch_tower/textures/wood_tower_col.png'),
+                    require('./res/watch_tower/textures/wood_tower_nor.png')]}
+        //materials={[require('./res/watch_tower/wooden watch tower2.mtl')]}
         position={[0, -1, -2]}
         scale={[.5, .5, .5]}
-        type="VRX"
-        dragType="FixedToWorld" onDrag={()=>{}}
-        onClick={this._onTappedIcecream}
-        animation={{name:"02", run:this.state.runAnimation, loop:true,}}
-      />*/
+        type="OBJ"
+      />
+    </ViroARScene>*/
